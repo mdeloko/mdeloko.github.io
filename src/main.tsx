74138aabcff3ae155187352c6a4d5754c35f14b6
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-01-22 18:32:06 -0300

    feat: adicionado React Router v7 e implementação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./pages/Home/Home.tsx";
+import Cursos from "./pages/Cursos/Cursos.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./pages/NotFound/NotFoundPage.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{ path: "/", element: <Home /> },
+	{ path: "*", element: <NotFoundPage /> },
+	{ path: "/cursos-e-formacoes", element: <Cursos /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
