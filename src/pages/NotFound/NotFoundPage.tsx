8da9c8d065cf6a72d75bf84d6276e49da9579a2e
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-01-31 01:40:43 -0300

    fix: adaptação para novo layout

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,18 +1,11 @@
-import NavBar from "../../components/NavBar/navbar";
-import Footer from "../../components/Footer/footer";
 import { Link } from "react-router-dom";
 import { MdKeyboardReturn } from "react-icons/md";
 import "./NotFound.css"
 
 export default function NotFoundPage() {
-	return (
-		<>
-			<NavBar />
-			<main>
-				<h1 id="not-found-title">❌ Página não encontrada!</h1>
-                <Link id="home" to={"/"} aria-label="Voltar página inicial"><MdKeyboardReturn/></Link>
-			</main>
-			<Footer />
-		</>
-	);
+	return (<>
+		<h1 id="not-found-title">❌ Página não encontrada!</h1>
+		<Link id="home" to={"/"} aria-label="Voltar página inicial"><MdKeyboardReturn/></Link>
+			
+	</>);
 }
