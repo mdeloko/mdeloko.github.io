7b1e78e2850c41be48577d82b4ef3b74f4f4ec4a
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-01-31 01:27:50 -0300

    feat: remoção de dados mockados e adição de conteúdo real.

--- src/pages/Cursos/Cursos.tsx
+++ src/pages/Cursos/Cursos.tsx
@@ -1,19 +1,59 @@
-import NavBar from "../../components/NavBar/navbar";
+import "./Cursos.css";
 import CourseCard from "../../components/CourseCard/card";
-import Footer from "../../components/Footer/footer";
-import Carousel from "../../components/Carousel/carousel";
+import Carousel, {type ICarouselProps} from "../../components/Carousel/carousel";
 
+const formacoesProps :ICarouselProps = {
+    carouselAriaLabel:"Carrossel de Cartões sobre Formações de Erik",
+    carouselAriaRoleDescription:"Carrossel",
+    nextButtonAriaLabel:"Ir para formação a frente",
+    previousButtonAriaLabel:"Voltar a formação anterior",
+    delay: 5500,
+    active:true
+}
+const cursosProps :ICarouselProps = {
+    carouselAriaLabel:"Carrossel de Cartões sobre cursos de Erik",
+    carouselAriaRoleDescription:"Carrossel",
+    nextButtonAriaLabel:"Ir para curso a frente",
+    previousButtonAriaLabel:"Voltar ao curso anterior",
+    delay: 3000,
+    active:true
+}
 
 export default function Cursos(){
     return(<>
-        <NavBar/>
-        <main>
-            <Carousel>
-                <CourseCard name="Técnico em Informática" grade="Técnico" institution="IFPR" start_date={new Date(2017,1,18)} conclusion_date={new Date("01-03-2022")}/>
-                <CourseCard name="Engenharia de Computação" grade="Gradução" institution="UTFPR" start_date={new Date(2022,2,1)}/>
-                <CourseCard name="Go Developer" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2025,10,1)} conclusion_date={new Date(2026,11,28)}/>
-            </Carousel>
-        </main>
-        <Footer/>
+        <h1>Formações:</h1>
+        <hr />
+        <Carousel
+            carouselAriaLabel={formacoesProps.carouselAriaLabel}
+            carouselAriaRoleDescription={formacoesProps.carouselAriaRoleDescription}
+            nextButtonAriaLabel={formacoesProps.nextButtonAriaLabel}
+            previousButtonAriaLabel={formacoesProps.previousButtonAriaLabel}
+            delay={formacoesProps.delay}
+            active={formacoesProps.active}
+        >
+            <CourseCard name="Engenharia de Computação" grade="Gradução" institution="UTFPR" start_date={new Date(2022,2,1)}/>
+            <CourseCard name="Técnico em Informática" grade="Técnico" institution="IFPR" start_date={new Date(2017,1,18)} conclusion_date={new Date("01-03-2022")}/>
+            
+        </Carousel>
+        <h1>Cursos e Bootcamps:</h1>
+        <hr />
+        <Carousel
+            carouselAriaLabel={cursosProps.carouselAriaLabel}
+            carouselAriaRoleDescription={cursosProps.carouselAriaRoleDescription}
+            nextButtonAriaLabel={cursosProps.nextButtonAriaLabel}
+            previousButtonAriaLabel={cursosProps.previousButtonAriaLabel}
+            delay={cursosProps.delay}
+            active={cursosProps.active}
+            playOnInit
+        >
+            <CourseCard name="Go Developer" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2025,10,1)} conclusion_date={new Date(2026,11,28)}/>
+            <CourseCard name="Linux Fundamentals" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2025,7,15)} conclusion_date={new Date(2025,8,19)}/>
+            <CourseCard name="Docker Fundamentals" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2025,5,10)} conclusion_date={new Date(2025,8,10)}/>
+            <CourseCard name="React Developer" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2024,11,1)} conclusion_date={new Date(2025,2,31)}/>
+            <CourseCard name="DevOps Fundamentals" grade="Curso" institution="Digital Innovation One (DIO)" start_date={new Date(2024,11,20)} conclusion_date={new Date(2025,0,21)}/>
+        </Carousel>
+        <h1>Stacks:</h1>
+        <hr />
+
     </>)
 }
