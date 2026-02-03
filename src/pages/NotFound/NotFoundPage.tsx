750a3d2db1f383e526a72903e53aa5e551147a2b
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-02-02 22:50:57 -0300

    fix/style: mudança para @ imports e remoção da página /* (not-found) das childrens da página / (home), que obrigou a implantação do Layout manualmente na página /*.

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import { MdKeyboardReturn } from "react-icons/md";
+import NavBar from "@/components/NavBar/navbar.tsx";
+import Footer from "@/components/Footer/footer.tsx";
+import ParticleComponent from "@/components/Particles/particles.tsx";
 import "./NotFound.css"
 
 export default function NotFoundPage() {
 	return (<>
-		<h1 id="not-found-title">❌ Página não encontrada!</h1>
-		<Link id="home" to={"/"} aria-label="Voltar página inicial"><MdKeyboardReturn/></Link>
-			
+		<ParticleComponent/>
+		<NavBar/>
+		<main className="pagina-not-found">
+			<h1 id="not-found-title">❌ Página não encontrada!</h1>
+			<Link id="home" to={"/"} aria-label="Voltar página inicial"><MdKeyboardReturn/></Link>
+		</main>
+		<Footer/>
 	</>);
 }
